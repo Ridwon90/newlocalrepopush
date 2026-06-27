d79f6604ac597a39b5ee8542d3cbce6fa423d988
Ridwon90 <ridwonelahi@gmail.com>
2026-06-27 16:59:44 +0600

    added something in main

--- branching2.c
+++ branching2.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
  int main() {
-     printf(new branch inside text!\n");
+     printf("new branch inside text!\n");
+    printf("this is changed in main branch\n");
      return 0;
  }
