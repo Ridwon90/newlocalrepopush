9014ff698e6ffc3214595969c49bcad039a31be4
Ridwon90 <ridwonelahi@gmail.com>
2026-06-27 17:25:11 +0600

    new conf

--- branching2.c
+++ branching2.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
  int main() {
      printf("new branch inside text!\n");
-    printf("this is changed in main branch\n");
+     printf("this was changed in main\n");
+    
      return 0;
  }
